dc99da990d4cf0eea3a630dfbdf0c8fda1425e5f
= <hardlane2000@gmail.com>
2023-08-05 19:12:13 +0200

    adding additional sudoku boards

--- data/sudoku_boards.py
+++ data/sudoku_boards.py
@@ -9,7 +9,12 @@ board_2 = [5, 3, None, 6, None, None, None, 9, 8, None, 7, None, 1, 9, 5, None, 
 board_3 = [None, None, 3, None, 6, None, 4, 9, None, None, None, None, 9, 8, None, None, 3, 1, 2, None, None, None, 4, 3, None, None, 6, 9, None, 7, None, 4, None, None, None, 5,None, None, None, None, 9, 8, 4, None, 
     7, 8, 6, None, None, None, None, 1, None, 9, 6, None, None, 5, None, 8, 2, None, 9,None, None, 3, 1, None, None, None, 5, 6,9, None, 5, None, 7, 2, None, 3, 8]
 
-boards = [board_1, board_2, board_3]
+board_4 = [1, None, None, 7, 3, None, None, None, None, 4, 8, 9, None, None, None, None, None, 1, None, None, 6, None, 4, None, 2, 9, 5, None, None, 7, 5, None, None, None, None, 6, 1, 2, None, 7, None, 3, None, 9, 5, 6, None, None, None, 
+    None, 8, 7, None, None, 9, 1, 4, None, 2, None, 8, None, None, 6, None, None, None, None, None, 5, 1, 2, None, None, None, None, 3, 7, None, None, 4]
+
+boards = [board_1, board_2, board_3, board_4]
+
+print(len(board_4))
 
 def get_board():
     return random.choice(boards)
